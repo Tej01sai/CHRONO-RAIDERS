547176a8df17580807cb4ea57c6d04a1f02dea97
Tej01sai <sai727868@gmail.com>
2025-04-22 00:52:53 +0530

    finall thing

--- frontend/src/utils/getContract.js
+++ frontend/src/utils/getContract.js
@@ -1,12 +1,12 @@
 import { BrowserProvider, Contract } from 'ethers';
-import deployedInfo from '../contracts/deployedInfo.json';
+import RelicNFT from '../contracts/RelicNFT.json';
 
 export const getContract = async () => {
   const provider = new BrowserProvider(window.ethereum);
   const signer = await provider.getSigner();
   const contract = new Contract(
-    deployedInfo.address,
-    deployedInfo.abi,
+    RelicNFT.address,
+    RelicNFT.abi,
     signer
   );
   return contract;
